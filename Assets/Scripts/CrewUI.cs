d3ae0d9324d375e191e1ffebfa8b6ccaf9ab9e98
Juan Carlos <jc.campbellg@gmail.com>
2020-10-25 13:43:53 -0600

    Add task list UI

--- Assets/Scripts/CrewUI.cs
+++ Assets/Scripts/CrewUI.cs
@@ -1,12 +1,15 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class CrewUI : MonoBehaviour
 {
 	public List<GameObject> allTasks;
 	public List<GameObject> tasksList;
 	public Settings settings;
+	public TextMeshProUGUI taskListUI;
+	bool showTask = true;
 
 	public void AssignTasks()
 	{
@@ -27,6 +30,32 @@ public class CrewUI : MonoBehaviour
 		foreach (GameObject taskObject in tasksAvalaible)
 		{
 			taskObject.GetComponent<TaskController>().HideTask();
+		}
+	}
+	private void Update()
+	{
+		if (Input.GetButtonDown("Task"))
+		{
+			showTask = !showTask;
+		}
+
+		taskListUI.text = "";
+
+		if (showTask)
+		{
+			foreach (GameObject task in tasksList)
+			{
+				taskListUI.text += task.GetComponent<TaskController>().task.label + "\n";
+			}
+		}
+
+		// Remove Done Tasks
+		foreach (GameObject task in tasksList)
+		{
+			if (task.layer == LayerMask.NameToLayer("Task Finish"))
+			{
+				tasksList.Remove(task);
+			}
 		}
 	}
 }
